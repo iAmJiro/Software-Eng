f364392051c57a4d066e7051a58a49f2b673a400
Stpnadi <137876888+Stpnadi@users.noreply.github.com>
2024-03-27 09:21:04 +1300

    React
    Setting up react folder

--- EasydayFolder/EasydayApi/obj/Debug/net8.0/EasydayApi.AssemblyInfo.cs
+++ EasydayFolder/EasydayApi/obj/Debug/net8.0/EasydayApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EasydayApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5b7a83fc0d63b740ec2d9b19a8efad0ed6a4111")]
 [assembly: System.Reflection.AssemblyProductAttribute("EasydayApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EasydayApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
